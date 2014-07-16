51b59bb6c4cf7b752052ffd23825090dca32e6b7
Marcus Tang <tmc9031@gmail.com>
2014-07-16 16:03:29 +0800

    up: static lib and shared lib

--- Modules.mk
+++ Modules.mk
@@ -1,5 +1,24 @@
 LOCAL_PATH := $(call my-dir)
 
+###################### libpreh ######################
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_LIBS := libpreh:libpreh.a
+
+# NOTE: Can strip only shared libraries or executables
+#LOCAL_STRIP_MODULE := true
+
+include $(BUILD_HOST_PREBUILT)
+
+###################### libpret ######################
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_LIBS := libpret:libpret.so
+
+LOCAL_STRIP_MODULE := true
+
+include $(BUILD_MULTI_PREBUILT)
+
 ###################### prehost ######################
 include $(CLEAR_VARS)
 
